124cec05230aea290c41337b57ccdb9b2905ea42
TonychenMomoRdp <46989981+TonychenMomoRdp@users.noreply.github.com>
2019-01-28 14:30:53 +0800

    Hello World!
    Hello World!

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import HelloWorld from "./components/HelloWorld";
 
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
+      <HelloWorld />
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
