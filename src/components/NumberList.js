7f7283018dc62a5abdce02a16e146692d597aade
TonychenMomoRdp <46989981+TonychenMomoRdp@users.noreply.github.com>
2019-01-30 13:21:48 +0800

    NumberList
    add NumberList components

---
+++ src/components/NumberList.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ListItem = props => {
+  return <li>{props.value}</li>;
+};
+
+const NumberList = props => {
+  const numbers = props.numbers;
+  const listItems = numbers.map(number => (
+    <ListItem key={number.toString()} value={number} />
+  ));
+  return <ul>{listItems}</ul>;
+};
+
+NumberList.defaultProps = {
+  numbers: [1, 2, 3, 4, 5]
+};
+
+export default NumberList;
