6282079234ab9e8136b844981b2f3de02b6549ac
TonychenMomoRdp <46989981+TonychenMomoRdp@users.noreply.github.com>
2019-01-30 13:58:40 +0800

    Blog
    add Blog components
    ex.Keys Must Only Be Unique Among Siblings

---
+++ src/components/Blog.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Blog = props => {
+  const sidebar = (
+    <ul>
+      {props.posts.map(post => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  );
+
+  const content = props.posts.map(post => (
+    <div key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content}</p>
+    </div>
+  ));
+
+  return (
+    <div>
+      {sidebar}
+      <hr />
+      {content}
+    </div>
+  );
+};
+
+Blog.defaultProps = {
+  posts: [
+    { id: 1, title: "Hello World", content: "Welcome to learning React!" },
+    { id: 2, title: "Installation", content: "You can install React from npm." }
+  ]
+};
+
+export default Blog;
